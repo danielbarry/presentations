9f159a8792c8c6c07a5a22cbc11d660583109be3
Dan <danbarry16@googlemail.com>
2016-11-03 17:04:23 +0000

    Basic git slides

--- git-basic/run.sh
+++ git-basic/run.sh
@@ -19,16 +19,9 @@ function run_cmd {
 function create_env {
   rm -rf out
   mkdir out
+  rm -rf wrk_dir
   mkdir wrk_dir
   cd wrk_dir
-}
-
-# remove_env()
-#
-# Removes the environment to be worked in.
-function remove_env {
-  cd ..
-  rm -rf wrk_dir
 }
 
 # test1()
@@ -50,6 +43,22 @@ function test2 {
 # Runs the test.
 function test3 {
   run_cmd "git remote add origin git@github.com:danielbarry/test.git"
+}
+
+# prep3()
+#
+# Prepares environment.
+function prep3 {
+  run_cmd 'git config --global push.default matching'
+  run_cmd 'git fetch'
+  run_cmd 'git pull origin master'
+  run_cmd 'rm -rf *.md *.txt'
+  run_cmd 'rm .git/index .git/refs/heads/master'
+  run_cmd 'echo "clean" > clean.txt'
+  run_cmd 'git add clean.txt'
+  run_cmd 'git commit -m "-- Repo Start --"'
+  run_cmd 'git branch --set-upstream-to=origin/master master'
+  run_cmd 'git push origin --force --all'
 }
 
 # test4()
@@ -108,6 +117,85 @@ function test11 {
   run_cmd "git push origin master"
 }
 
+# test12()
+#
+# Runs the test.
+function test12 {
+  run_cmd "git status"
+}
+
+# test14()
+#
+# Runs the test.
+function test14 {
+  run_cmd "echo -e '\n\nNew words' > readme.md; git status"
+}
+
+# test15()
+#
+# Runs the test.
+function test15 {
+  run_cmd "git stash"
+}
+
+# test16()
+#
+# Runs the test.
+function test16 {
+  run_cmd "git status"
+}
+
+# test17()
+#
+# Runs the test.
+function test17 {
+  run_cmd "git stash pop"
+}
+
+# prep17()
+#
+# Prepares environment.
+function prep17 {
+  run_cmd 'cp -r .git .git.old'
+  run_cmd 'git stash'
+  run_cmd 'echo "New data" > new.txt'
+  run_cmd 'git add new.txt'
+  run_cmd 'git commit -m "New data"'
+  run_cmd 'git push'
+  run_cmd 'git stash pop'
+  run_cmd 'rm -rf .git'
+  run_cmd 'mv .git.old .git'
+  run_cmd 'rm new.txt'
+}
+
+# test18()
+#
+# Runs the test.
+function test18 {
+  run_cmd "git fetch"
+}
+
+# test19()
+#
+# Runs the test.
+function test19 {
+  run_cmd "git stash"
+}
+
+# test20()
+#
+# Runs the test.
+function test20 {
+  run_cmd "git pull"
+}
+
+# test21()
+#
+# Runs the test.
+function test21 {
+  run_cmd "git stash pop"
+}
+
 # main()
 #
 # The main directory to run the various tests.
@@ -115,26 +203,38 @@ function main {
   # Create the environment
   create_env
   # Run the tests
-  test1 &> ../out/test1.txt
-  test2 &> ../out/test2.txt
-  test3 &> ../out/test3.txt
+  test1 > ../out/test1.txt 2>&1
+  test2 > ../out/test2.txt 2>&1
+  test3 > ../out/test3.txt 2>&1
   # NOTE: Here we sneakily wipe the history completely.
-  git config --global push.default matching
-  git fetch
-  git pull origin master
-  git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *' --prune-empty --tag-name-filter cat -- --all
-  git push origin --force --all
-  test4 &> ../out/test4.txt
-  test5 &> ../out/test5.txt
-  test6 &> ../out/test6.txt
-  test7 &> ../out/test7.txt
-  test8 &> ../out/test8.txt
-  test9 &> ../out/test9.txt
-  test10 &> ../out/test10.txt
-  test11 &> ../out/test11.txt
-  # Remove the environment
-  remove_env
+  prep3 > ../out/prep3.txt 2>&1
+  test4 > ../out/test4.txt 2>&1
+  test5 > ../out/test5.txt 2>&1
+  test6 > ../out/test6.txt 2>&1
+  test7 > ../out/test7.txt 2>&1
+  test8 > ../out/test8.txt 2>&1
+  test9 > ../out/test9.txt 2>&1
+  test10 > ../out/test10.txt 2>&1
+  test11 > ../out/test11.txt 2>&1
+  test12 > ../out/test12.txt 2>&1
+  test14 > ../out/test14.txt 2>&1
+  test15 > ../out/test15.txt 2>&1
+  test16 > ../out/test16.txt 2>&1
+  test17 > ../out/test17.txt 2>&1
+  # NOTE: Here we sneakily push a commit and forget we did it.
+  prep17 > ../out/prep17.txt 2>&1
+  test18 > ../out/test18.txt 2>&1
+  test19 > ../out/test19.txt 2>&1
+  test20 > ../out/test20.txt 2>&1
+  test21 > ../out/test21.txt 2>&1
   # Compile and view presentation
+  cd ..
+  rm presentation.aux
+  rm presentation.log
+  rm presentation.nav
+  rm presentation.out
+  rm presentation.snm
+  rm presentation.toc
   pdflatex presentation.tex
   evince presentation.pdf
 }
