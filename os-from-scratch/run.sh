5d142cdbf80b74293014615e825799ba688ce5f9
Dan <danbarry16@googlemail.com>
2017-03-31 03:40:42 +0100

    HTML to PDF

--- os-from-scratch/run.sh
+++ os-from-scratch/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 pandoc --self-contained --standalone --toc --number-sections --section-divs -c style.css -o output.html book.md
+wkhtmltopdf -B 4cm -T 4cm -L 1.5cm -R 1.5cm --minimum-font-size 16 -s A5 -l output.html os-from-scratch-$(date "+%d-%m-%Y").pdf
