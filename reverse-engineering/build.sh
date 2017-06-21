7b7c110cca9296544a7307ffa1f7fad0122b5797
Dan <danbarry16@googlemail.com>
2017-06-22 09:04:02 +1200

    Build script clean-up after build

--- reverse-engineering/build.sh
+++ reverse-engineering/build.sh
@@ -1,3 +1,7 @@
+# Build the documents
 pandoc -t beamer -o talk.tex talk.md
 pdflatex main.tex
 pdflatex main.tex
+# Clean-up after ourselves
+rm *.aux *.log *.nav *.out *.snm *.toc *.vrb
+rm talk.tex
